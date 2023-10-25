f72a4ebcb9606584209ebfaac4197db6105a5ccf
Faizul <fzlxtech>
2023-10-26 03:58:39 +0800

    update

--- src/commands/bodoh.js
+++ src/commands/bodoh.js
@@ -4,19 +4,48 @@ require('dotenv').config()
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('bodoh')
-		.setDescription('Siapa bodoh!'),
+		.setDescription('Siapa bodoh?'),
 	async execute(interaction) {
-		await interaction.guild.members.fetch();
-		const channel = await interaction.member.guild.channels.fetch("945259898860617758");
-		const members = channel.members.map(member => member.id);
-
-		if (members.length > 0) {
-			var randomWord = ['eh', 'hehe', 'hihi'];
-			var replyTo = members[Math.floor(Math.random()*members.length)];
-			var randomWordReply = randomWord[Math.floor(Math.random()*randomWord.length)];
-			interaction.reply(randomWordReply + ' ' + userMention(replyTo) + ' bodoh');
-		} else {
-			interaction.reply('Takde orang lagi');
+		await interaction.deferReply()
+		const members = await getData(interaction)
+		if (members.length === 0) {
+			await interaction.editReply('Masukle channel dulu')
 		}
+
+		members.forEach(m => {
+			m.members.forEach(async ms => {
+				if (ms === interaction.user.id)  {
+					var randomWord = ['eh', 'hehe', 'hihi'];
+					var replyTo = m.members[Math.floor(Math.random()*m.members.length)];
+					var randomWordReply = randomWord[Math.floor(Math.random()*randomWord.length)];
+					await interaction.editReply(randomWordReply + ' ' + userMention(replyTo) + ' bodoh')
+				}
+			});
+		});
 	},
 };
+
+async function getData(interaction) {
+	const channels = await interaction.member.guild.channels.fetch()
+	let allMemberInChannel = []
+	channels.forEach(async element => {
+		const channel = await interaction.member.guild.channels.fetch(element.id.toString());
+		if (channel.type === 2) {
+			if (channel.members !== undefined) {
+				let membersID = []
+				channel.members.forEach(e => {
+					membersID.push(e.id)
+				});
+
+				if (membersID.length > 0)  {
+					allMemberInChannel.push({
+						channel: element.id,
+						members: membersID
+					})
+				}
+			}
+		}
+	});
+
+	return allMemberInChannel
+}
